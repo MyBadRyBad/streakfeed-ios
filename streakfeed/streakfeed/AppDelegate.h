f4d1635d15306e7bd95ad818f9372561eb42ae61
Ryan Badilla <pickledprotoast2@gmail.com>
2016-06-20 20:02:54 -0400

    added project

---
+++ streakfeed/streakfeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  streakfeed
+//
+//  Created by Ryan Badilla on 6/20/16.
+//  Copyright © 2016 rybadilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ streakfeed/streakfeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  streakfeed
+//
+//  Created by Ryan Badilla on 6/20/16.
+//  Copyright © 2016 rybadilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
